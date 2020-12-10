fb299193ec7025a44c7365b7744c4aac51bdd163
lwedwards3 <lwedwards3@gmail.com>
2020-12-10 12:48:43 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,30 @@
+from machine import Pin
+import utime
+
+class Test:
+    
+    def __init__(self):
+        self.LED = Pin(2, Pin.OUT)
+        self.LED.value(1)
+        self.sleep_time_ms = 500
+        self.start_time = utime.ticks_ms()
+        
+        
+    def check_time(self):
+        if utime.ticks_ms() < self.start_time:
+            self.start_time = 0
+            return False
+        return utime.ticks_ms() > (self.start_time + self.sleep_time_ms)
+    
+    
+    def blink(self):
+        self.LED.value(not self.LED.value())
+        self.start_time = utime.ticks_ms()
+    
+    
+    
+
+    def run(self):
+        if self.check_time():
+            self.blink()
+        
